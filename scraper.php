d041a8c26bc7fd33ba41664ae86ca39617e2a559
vikashbaria <vikashbaria4@gmail.com>
2018-01-29 12:50:19 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,8 +2,15 @@
 // This is a template for a PHP scraper on morph.io (https://morph.io)
 // including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+$Links	=	array('https://www.redfin.com/county/536/GA/Cobb-County/filter/include=sold-1wk',
+             'https://www.redfin.com/county/559/GA/Fayette-County/filter/include=sold-1wk'
+             );
+for ($mainpage = 0; $mainpage < sizeof($Links); $mainpage++)
+{
+  echo "$mainpage++"; 
+}
 //
 // // Read in a page
 // $html = scraperwiki::scrape("http://foo.com");
