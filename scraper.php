352684e290a7a2afdb8a34f71debfb14d8d05a99
vikashbaria <vikashbaria4@gmail.com>
2018-01-29 15:08:20 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,12 +1,17 @@
 <?
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-$Links	=	array('https://www.redfin.com/county/536/GA/Cobb-County/filter/include=sold-1wk',
-             'https://www.redfin.com/county/559/GA/Fayette-County/filter/include=sold-1wk'
+$Links	=	array('https://www.redfin.com/county/559/GA/Fayette-County/filter/include=sold-1wk',
+'https://www.redfin.com/county/536/GA/Cobb-County/filter/include=sold-1wk'            
              );
 for ($mainpage = 0; $mainpage < sizeof($Links); $mainpage++)
 {
-  echo "$Links[$mainpage]\n"; 
+  $html	=	file_get_contents($Links[$mainpage]); 
+  
+  if($html)
+  {
+	  echo $html;
+  }
 }
 
 ?>
